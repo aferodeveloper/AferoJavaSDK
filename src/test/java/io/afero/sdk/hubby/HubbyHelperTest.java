3b54a0512fc4b2a9f09234c0ced19ab656a8d374
Tony Myles <tony@kiban.io>
2017-06-23 17:44:57 -0700

    ANDROID-1095: SDK should support secure-softhub
    Pretty much a complete rewrite HubbyHelper for secure soft-hub.
    Added HubbyHelperTest framework and verify the startup flow. More tests coming soon.
    Changed MainActivity to comply with new HubbyHelper interface.

---
+++ src/test/java/io/afero/sdk/hubby/HubbyHelperTest.java
@@ -0,0 +1,329 @@
+/*
+ * Copyright (c) 2014-2017 Afero, Inc. All rights reserved.
+ */
+
+package io.afero.sdk.hubby;
+
+import android.app.Activity;
+import android.content.Context;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.HashMap;
+
+import io.afero.sdk.client.afero.AferoClient;
+import io.afero.sdk.client.afero.models.ActionResponse;
+import io.afero.sdk.client.afero.models.ConclaveAccessDetails;
+import io.afero.sdk.client.afero.models.DeviceAssociateResponse;
+import io.afero.sdk.client.afero.models.DeviceRequest;
+import io.afero.sdk.client.afero.models.Location;
+import io.afero.sdk.client.afero.models.PostActionBody;
+import io.afero.sdk.client.afero.models.RequestResponse;
+import io.afero.sdk.device.DeviceModel;
+import io.afero.sdk.device.DeviceProfile;
+import io.kiban.hubby.Hubby;
+import io.kiban.hubby.NotificationCallback;
+import rx.Observable;
+import rx.Observer;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class HubbyHelperTest {
+
+    @After
+    public void hubbyHelperReleaseInstance() {
+        HubbyHelper.releaseInstance();
+    }
+
+    @Test
+    public void init() throws Exception {
+        makeHubbyHelperTester()
+            .acquireInstance()
+            .verifyHubbyHelperCreated();
+    }
+
+    @Test
+    public void start() throws Exception {
+        makeHubbyHelperTester()
+            .acquireInstance()
+
+            .startHubby()
+            .waitUntilStartedCalled()
+
+            .verifyHubbyIsStarting()
+            .verifyHubbyNotRunning()
+
+            .hubbyInitializationCompletedCallback()
+            .waitUntilStartCompleted()
+
+            .verifyHubbyNotStarting()
+            .verifyHubbyIsRunning()
+            .verifyHubbyStartCompleted()
+        ;
+    }
+
+    @Test
+    public void stop() throws Exception {
+        makeHubbyHelperTester()
+            .acquireInstance()
+            .verifyHubbyNotRunning();
+    }
+
+    @Test
+    public void onPause() throws Exception {
+
+    }
+
+    @Test
+    public void onResume() throws Exception {
+
+    }
+
+    @Test
+    public void isActive() throws Exception {
+
+    }
+
+    @Test
+    public void isRunning() throws Exception {
+
+    }
+
+    @Test
+    public void isStarting() throws Exception {
+
+    }
+
+    @Test
+    public void setService() throws Exception {
+
+    }
+
+    // test support --------------------------------------------------------
+
+    private HubbyHelperTester makeHubbyHelperTester() {
+        return new HubbyHelperTester();
+    }
+
+    private class HubbyHelperTester {
+        final Activity activity;
+        final MockAferoClient aferoClient = new MockAferoClient();;
+        final MockHubbyImpl hubbyImpl = new MockHubbyImpl();
+        final StartObserver startObserver = new StartObserver();
+
+        HubbyHelper hubbyHelper;
+
+        HubbyHelperTester() {
+            activity = Robolectric.buildActivity(Activity.class).create().get();
+        }
+
+        HubbyHelperTester acquireInstance() {
+            hubbyHelper = HubbyHelper.acquireInstance(activity, aferoClient);
+            hubbyHelper.setHubbyImpl(hubbyImpl);
+            return this;
+        }
+
+        HubbyHelperTester startHubby() {
+            hubbyHelper.start().subscribe(startObserver);
+            return this;
+        }
+
+        HubbyHelperTester waitUntilStartedCalled() {
+            hubbyImpl.waitUntilStartedCalled();
+            return this;
+        }
+
+        HubbyHelperTester waitUntilStartCompleted() {
+            startObserver.waitUntilCompleted();
+            return this;
+        }
+
+        HubbyHelperTester stopHubby() {
+            hubbyHelper.stop();
+            return this;
+        }
+
+        HubbyHelperTester hubbyInitializationCompletedCallback() {
+            hubbyImpl.callbackInitializationCompleted();
+            return this;
+        }
+
+        HubbyHelperTester verifyHubbyHelperCreated() {
+            assertNotNull(hubbyHelper);
+            return this;
+        }
+
+        HubbyHelperTester verifyHubbyStartCompleted() {
+            assertTrue(startObserver.isCompleted);
+            return this;
+        }
+
+        HubbyHelperTester verifyHubbyIsStarting() {
+            assertTrue(hubbyHelper.isStarting());
+            return this;
+        }
+
+        HubbyHelperTester verifyHubbyNotStarting() {
+            assertFalse(hubbyHelper.isStarting());
+            return this;
+        }
+
+        HubbyHelperTester verifyHubbyIsRunning() {
+            assertTrue(hubbyHelper.isRunning());
+            return this;
+        }
+
+        HubbyHelperTester verifyHubbyNotRunning() {
+            assertFalse(hubbyHelper.isRunning());
+            return this;
+        }
+    }
+
+    private class MockHubbyImpl implements HubbyHelper.HubbyImpl {
+
+        private Context mContext;
+        private HashMap<Hubby.Config, String> mConfigs;
+        private NotificationCallback mCallback;
+        private final Object mStartLock = new Object();
+
+        @Override
+        public void initialize(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        public void start(HashMap<Hubby.Config, String> configs, final NotificationCallback callback) {
+            mConfigs = configs;
+            mCallback = callback;
+
+            synchronized (mStartLock) {
+                mStartLock.notifyAll();
+            }
+        }
+
+        @Override
+        public void stop() {
+
+        }
+
+        void callbackInitializationCompleted() {
+            mCallback.initializationComplete();
+        }
+
+        void waitUntilStartedCalled() {
+            synchronized (mStartLock) {
+                try {
+                    mStartLock.wait(5000);
+                } catch (InterruptedException e) {
+                    assertTrue("waitUntilStartedCalled timed out", false);
+                }
+            }
+        }
+    };
+
+    class MockAferoClient implements AferoClient {
+
+        @Override
+        public Observable<ActionResponse> postAttributeWrite(DeviceModel deviceModel, PostActionBody body, int maxRetryCount, int statusCode) {
+            return null;
+        }
+
+        @Override
+        public Observable<RequestResponse[]> postBatchAttributeWrite(DeviceModel deviceModel, DeviceRequest[] body, int maxRetryCount, int statusCode) {
+            return null;
+        }
+
+        @Override
+        public Observable<DeviceProfile> getDeviceProfile(String profileId, String locale, ImageSize imageSize) {
+            return null;
+        }
+
+        @Override
+        public Observable<DeviceProfile[]> getAccountDeviceProfiles(String locale, ImageSize imageSize) {
+            return null;
+        }
+
+        @Override
+        public Observable<ConclaveAccessDetails> postConclaveAccess(String mobileClientId) {
+            return null;
+        }
+
+        @Override
+        public Observable<Location> putDeviceLocation(String deviceId, Location location) {
+            return null;
+        }
+
+        @Override
+        public Observable<Location> getDeviceLocation(DeviceModel deviceModel) {
+            return null;
+        }
+
+        @Override
+        public Observable<DeviceAssociateResponse> deviceAssociate(String associationId, boolean isOwnershipVerified, String locale, ImageSize imageSize) {
+            return null;
+        }
+
+        @Override
+        public Observable<DeviceModel> deviceDisassociate(DeviceModel deviceModel) {
+            return null;
+        }
+
+        @Override
+        public String getActiveAccountId() {
+            return "account-id";
+        }
+
+        @Override
+        public int getStatusCode(Throwable t) {
+            return 0;
+        }
+
+        @Override
+        public boolean isTransferVerificationError(Throwable t) {
+            return false;
+        }
+    }
+
+    static class StartObserver implements Observer<HubbyHelper> {
+
+        boolean isCompleted;
+        final Object completedLock = new Object();
+
+        @Override
+        public void onCompleted() {
+            synchronized (completedLock) {
+                isCompleted = true;
+                completedLock.notifyAll();
+            }
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            assertTrue(false);
+        }
+
+        @Override
+        public void onNext(HubbyHelper hubbyHelper) {
+        }
+
+        public void waitUntilCompleted() {
+            synchronized (completedLock) {
+                try {
+                    completedLock.wait(1000);
+                } catch (InterruptedException e) {
+                    assertTrue(false);
+                }
+            }
+        }
+    }
+
+}
