4cf5a361a8951d6e1e1874518b85f16c2354eb8b
Tony Myles <tony@kiban.io>
2019-02-01 08:36:00 -0800

    Expose several new client api calls to make our partner's lives easier. The following areas should now be covered:   - Create Account   - Device Rules & Schedules   - Account Sharing
    Higher level utility classes `RuleCollection`, `DeviceRuleCollection`, and `RuleBuilder` have been added to manage rules.

---
+++ afero-sdk-core/src/main/java/io/afero/sdk/client/afero/models/AccountPrivileges.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2014-2017 Afero, Inc. All rights reserved.
+ */
+
+package io.afero.sdk.client.afero.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AccountPrivileges {
+    public boolean canWrite;
+    public boolean owner;
+}
