6d90b0aac13cfa25c7d815037d6d665b4018eb97
Martin Arnberg <martin@afero.io>
2023-01-26 12:45:24 -0800

    serialize and save the complete AccessToken

--- samples/afero-lab/app/src/main/java/io/afero/aferolab/helper/PrefsHelper.java
+++ samples/afero-lab/app/src/main/java/io/afero/aferolab/helper/PrefsHelper.java
@@ -7,6 +7,17 @@ package io.afero.aferolab.helper;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+import io.afero.sdk.client.retrofit2.models.AccessToken;
+import io.afero.sdk.device.DeviceProfile;
+import io.afero.sdk.log.AfLog;
+
 public class PrefsHelper {
 
     private static final String SHARED_PREFS_ACCOUNT = "shared_prefs_account";
@@ -21,12 +32,18 @@ public class PrefsHelper {
         return context.getSharedPreferences(SHARED_PREFS_ACCOUNT, Context.MODE_PRIVATE);
     }
 
-    public static String getAccessToken(Context context) {
-        return getAccountSharedPrefs(context).getString(ACCOUNT_PREFS_ACCESS_TOKEN, "");
-    }
+    public static AccessToken getAccessToken(Context context) {
+        try {
+            String tokenJsonString = getAccountSharedPrefs(context).getString(ACCOUNT_PREFS_ACCESS_TOKEN, "");
+            ObjectMapper mapper = new ObjectMapper();
+            AccessToken token = mapper.readValue(tokenJsonString, AccessToken.class);
+            AfLog.d("#### Get token from prefs: " + token);
 
-    public static String getRefreshToken(Context context) {
-        return getAccountSharedPrefs(context).getString(ACCOUNT_PREFS_REFRESH_TOKEN, "");
+            return token;
+        } catch (IOException e) {
+            AfLog.d("Unable to get access token " + e.toString());
+            return null;
+        }
     }
 
     public static String getUserId(Context context) {
@@ -41,16 +58,20 @@ public class PrefsHelper {
         return getAccountSharedPrefs(context).getString(ACCOUNT_PREFS_ACCOUNT_NAME, "");
     }
 
-    public static void saveAccessToken(Context context, String token) {
-        getAccountSharedPrefs(context).edit()
-            .putString(ACCOUNT_PREFS_ACCESS_TOKEN, token)
-            .apply();
-    }
 
-    public static void saveRefreshToken(Context context, String token) {
-        getAccountSharedPrefs(context).edit()
-            .putString(ACCOUNT_PREFS_REFRESH_TOKEN, token)
+
+    public static void saveAccessToken(Context context, AccessToken token) {
+        try {
+            //Convert token to JSON string
+            ObjectMapper mapper = new ObjectMapper();
+            String tokenJsonString = mapper.writeValueAsString(token);
+            AfLog.d("####  Save Token to prefs: " + tokenJsonString);
+            getAccountSharedPrefs(context).edit()
+            .putString(ACCOUNT_PREFS_ACCESS_TOKEN, tokenJsonString)
             .apply();
+        } catch (Exception e) {
+            AfLog.d("Unable to save access token " + e.toString());
+        }
     }
 
     public static void saveAccountId(Context context, String id) {
